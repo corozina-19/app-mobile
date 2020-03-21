c3a474fec10dde820f15727ec9ee4d4e5e795046
pattyzamsal <pazs1991@gmail.com>
2020-03-21 15:33:27 -0500

    Create a new project in Xcode

---
+++ Covid19App/Covid19App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid19App
+//
+//  Created by Patricia Zambrano on 21/03/20.
+//  Copyright © 2020 Patricia Zambrano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
